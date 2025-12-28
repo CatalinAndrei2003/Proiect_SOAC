c367f254ac5bed027ac388c9dedfb46d71f29788
deodorant9999 <161837292+deodorant9999@users.noreply.github.com>
2025-12-29 00:59:15 +0200

    o incercare

--- Initial Prototype/ConsoleApp1/Program.cs
+++ Initial Prototype/ConsoleApp1/Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string filePath = "Date_Intrare/FBUBBLE.TRA";
+            List<Instructiune> Instructiuni = new List<Instructiune>();
+            foreach (string line in File.ReadLines(filePath))
+            {
+                string[] lineText = line.Split(' ');
+                Instructiune instructiune = new Instructiune(lineText[0],
+                                                            int.Parse(lineText[1]), 
+                                                            int.Parse(lineText[2]));
+                Instructiuni.Add(instructiune);
+            }
+            foreach( Instructiune instructiune in Instructiuni)
+            {
+                Console.WriteLine("{0} {1} {2}",instructiune.tipInstructiune,instructiune.PC,instructiune.Destinatie);
+                
+            }
         }
     }
 }
